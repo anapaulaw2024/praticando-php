f6299342bbdbe1966e16b4793c9e0219652671ec
AnaWulCard <156938112+anapaulaw2024@users.noreply.github.com>
2025-05-26 10:29:18 -0300

    Add files via upload

---
+++ exUm.php
@@ -0,0 +1,12 @@
+<?php
+print("Comparando preços de um gabinete de 510GB de RAM 1.5 SSD 510 em dois sites específicos(preços fictícios)<br><br>");
+$preco_1 = 500;
+$preço_2 = 900;
+
+if($preco_1<$preço_2){
+    print("O primeiro é melhor!<br>");
+}else{
+    print("O segundo é melhor<br>");
+}
+
+?>
