3c93c8af6fadef190763cf9d15d7ee4d1bf4a522
AnaWulCard <156938112+anapaulaw2024@users.noreply.github.com>
2026-05-19 09:17:52 -0300

    Add echo_print.php for demonstrating PHP print and echo

---
+++ echo_print.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Comandos print e echo em PHP</title>
+
+</head>
+<body>
+  <?php
+echo "Comando echo<br/>";
+print "comando print<hr/>";
+?>
+</body>
+</html>
