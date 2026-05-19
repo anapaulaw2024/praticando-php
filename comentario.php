6445f9a9d651f55c4fae0d080c22538926653088
AnaWulCard <156938112+anapaulaw2024@users.noreply.github.com>
2026-05-19 09:20:16 -0300

    Add comentario.php with PHP echo and print examples

---
+++ comentario.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Comandos print e echo em PHP</title>
+
+</head>
+<body>
+  <?php
+//comentario uma linha :)
+echo "Comando echo<br/>";
+#comentário uma linha shell :)
+
+print "comando print<hr/>";
+/*Comentário vida louca
+varias linhas */
+#alguem
+#me
+#contrata
+#pfvr
+//aaaaaaaaaaaa
+?>
+</body>
+</html>
