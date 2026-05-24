6d645583a0a7f26498aae5dd7702c3d71329e7ff
AnaWulCard <156938112+anapaulaw2024@users.noreply.github.com>
2026-05-24 10:26:04 -0300

    Create ifelse_op_logicos.php

---
+++ ifelse_op_logicos.php
@@ -0,0 +1,24 @@
+!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>IF e ELSE - Operadores Lógicos</title>
+</head>
+<body>
+  <?php
+// && - e
+// || - ou
+//XOR - verdadeiro se apenas uma das expressões forem verdadeiras e a outra falsa
+// ! - NEGACAO (inverte um resultado de uma expressao)
+// () estabelecer precedencia
+if(3 == 3 XOR 10 ==15) {
+  echo "verdadeiro";
+}else{
+  echo "falso";
+  }
+
+?>
+    
+</body>
+</html>
